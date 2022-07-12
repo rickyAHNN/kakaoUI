306ceb3ee775486871f8d9de17b058d8b76543ca
안정호 <smrmaster88@gmail.com>
2022-07-12 12:40:30 +0900

    kakaoUI

--- lib/kakao/secondPage.dart
+++ lib/kakao/secondPage.dart
@@ -1,3 +1,5 @@
+// GIT token : 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
+
 import 'package:flutter/material.dart';
 
 class secondPage extends StatelessWidget {
@@ -34,7 +36,74 @@ class secondPage extends StatelessWidget {
         itemCount: 20,
         itemBuilder: (BuildContext context, int index) {
           return Container(
-            child: Text('df'),
+            child: Padding(
+              padding:
+                  const EdgeInsets.only(top: 5, bottom: 5, left: 5, right: 5),
+              child: Row(
+                children: [
+                  CircleAvatar(
+                    radius: 40,
+                    backgroundColor: Colors.transparent,
+                    backgroundImage: NetworkImage(
+                        'https://dimg.donga.com/wps/NEWS/IMAGE/2015/04/17/70766104.3.jpg'),
+                  ),
+                  SizedBox(width: 5),
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Row(
+                        children: [
+                          Text(
+                            '농심 너구리',
+                            style: TextStyle(
+                                fontSize: 15,
+                                color: Colors.black87,
+                                fontWeight: FontWeight.w800),
+                          ),
+                          SizedBox(width: 5),
+                          Text('466'),
+                          SizedBox(width: 5),
+                          Icon(
+                            Icons.alarm_off,
+                            size: 15,
+                          ),
+                        ],
+                      ),
+                      Text(
+                        '채팅글 채팅글',
+                        style: TextStyle(
+                            fontSize: 20, fontWeight: FontWeight.w200),
+                      ),
+                    ],
+                  ),
+                  SizedBox(width: 130),
+                  Expanded(
+                    child: Column(
+                      children: [
+                        Text(
+                          '오후 11:38',
+                          style: TextStyle(fontSize: 10),
+                        ),
+                        SizedBox(width: 5),
+                        Stack(
+                          children: [
+                            CircleAvatar(
+                              backgroundColor: Colors.red,
+                              radius: 13,
+                              child: Text(
+                                '100',
+                                style: TextStyle(
+                                    color: Colors.white, fontSize: 13),
+                              ),
+                            ),
+                          ],
+                        ),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+            ),
             color: Colors.white,
           );
         },
